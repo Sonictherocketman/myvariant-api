15f7af4cc5330f44e63926c376ddf6b60411bab9
Brian Schrader <sonictherocketman@gmail.com>
2015-06-26 17:07:55 -0700

    Removed pyc files.

--- setup.py
+++ setup.py
@@ -1,1 +1,17 @@
+from setuptools import setup
 
+
+setup(
+    name='myvariant-api',
+    version='1.0',
+    author='Brian Schrader',
+    author_email='brian@biteofanapple.com',
+    packages=['myvariant', 'tests'],
+    scripts=[], # TODO: Add bin/ scripts here.
+    license='LICENSE.txt',
+    description='A simple API wrapper for the MyVariant.info API.',
+    keywords=['variant', 'genomics'],
+    install_requires=[
+        'requests'
+        ]
+    )
